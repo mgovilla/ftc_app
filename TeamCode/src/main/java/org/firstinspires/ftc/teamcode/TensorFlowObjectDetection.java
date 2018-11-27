f289465a9d9dd6f1a09158ccc1c8599570129deb
mgovilla <mgovilla@gmail.com>
2018-11-26 20:34:12 -0500

    Logic Change to 2 minerals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetection.java
@@ -113,38 +113,50 @@ public class TensorFlowObjectDetection extends LinearOpMode {
                     // the last time that call was made.
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                     if (updatedRecognitions != null) {
-                      telemetry.addData("# Object Detected", updatedRecognitions.size());
-                      if (updatedRecognitions.size() == 3) {
-                        int goldMineralX = -1;
-                        int silverMineral1X = -1;
-                        int silverMineral2X = -1;
-                        for (Recognition recognition : updatedRecognitions) {
-                          if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
-                            goldMineralX = (int) recognition.getLeft();
-                          } else if (silverMineral1X == -1) {
-                            silverMineral1X = (int) recognition.getLeft();
-                          } else {
-                            silverMineral2X = (int) recognition.getLeft();
-                          }
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        if (updatedRecognitions.size() == 2) {
+
+                            int[] minerals = new int[2];
+
+                            for (int i = 0; i < 2; i++) {
+                                minerals[i] = (int) updatedRecognitions.get(i).getLeft();
+                            }
+
+                            if (minerals[1] > minerals[0]) {
+                            /*
+                                    Mineral 1 is to the right of mineral 0
+                             */
+
+                                if (updatedRecognitions.get(1).getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    telemetry.addData("Gold Mineral Position", "Center");
+                                } else if (updatedRecognitions.get(0).getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    telemetry.addData("Gold Mineral Position", "Left");
+                                } else {
+                                    telemetry.addData("Gold Mineral Position", "Right");
+                                }
+                            } else {
+                            /*
+                                    Mineral 0 is to the right of mineral 1
+                             */
+
+                                if (updatedRecognitions.get(0).getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    telemetry.addData("Gold Mineral Position", "Center");
+                                } else if (updatedRecognitions.get(1).getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    telemetry.addData("Gold Mineral Position", "Left");
+                                } else {
+                                    telemetry.addData("Gold Mineral Position", "Right");
+                                }
+                            }
+
+                            telemetry.update();
                         }
-                        if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
-                          if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
-                            telemetry.addData("Gold Mineral Position", "Left");
-                          } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
-                            telemetry.addData("Gold Mineral Position", "Right");
-                          } else {
-                            telemetry.addData("Gold Mineral Position", "Center");
-                          }
-                        }
-                      }
-                      telemetry.update();
                     }
                 }
             }
-        }
 
-        if (tfod != null) {
-            tfod.shutdown();
+            if (tfod != null) {
+                tfod.shutdown();
+            }
         }
     }
 
@@ -158,7 +170,7 @@ public class TensorFlowObjectDetection extends LinearOpMode {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = CameraDirection.BACK;
+        parameters.cameraDirection = CameraDirection.FRONT;
 
         //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
@@ -171,7 +183,7 @@ public class TensorFlowObjectDetection extends LinearOpMode {
      */
     private void initTfod() {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-            "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
